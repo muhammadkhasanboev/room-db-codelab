1a96095391a8cef831381c4eecac70639cb451de
Muhammad <muhammadxasanboyev1234@gmail.com>
2025-09-19 09:44:43 +0500

    some changes

--- app/src/main/java/com/example/inventory/data/OfflineItemsRepository.kt
+++ app/src/main/java/com/example/inventory/data/OfflineItemsRepository.kt
@@ -25,4 +25,5 @@ class OfflineItemsRepository(private val itemDao: ItemDao) : ItemsRepository{
     override suspend fun insertItem(item: Item) = itemDao.insert(item)
     override suspend fun deleteItem(item: Item) = itemDao.delete(item)
     override suspend fun updateItem(item: Item) = itemDao.update(item)
+
 }
